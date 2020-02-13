ecb10dad507850c115b21b059bb309700290feb2
Oscar Froberg <oscar@agoodid.se>
2020-02-13 16:00:32 +0100

    Added a gitignore. Pep.

--- wp-content/themes/astra-child/agoodblock.php
+++ wp-content/themes/astra-child/agoodblock.php
@@ -6,74 +6,74 @@
         
         	<?php include('agoodblock/1-col.php'); ?>
         	
-		<?php elseif( get_row_layout() == '2_col' ): ?>
-		
-			<?php include('agoodblock/2-col.php'); ?>
-	    
-	    <?php elseif( get_row_layout() == '2_col_wysiwyg' ): ?>
+				<?php elseif( get_row_layout() == '2_col' ): ?>
+				
+					<?php include('agoodblock/2-col.php'); ?>
+					
+				<?php elseif( get_row_layout() == '2_col_wysiwyg' ): ?>
+					
+					<?php include('agoodblock/2-col-wysiwyg.php'); ?>
+				
+				<?php elseif( get_row_layout() == 'citat' ): ?>
+				
+					<?php include('agoodblock/citat.php'); ?>
+					
+				<?php elseif( get_row_layout() == 'code' ): ?>
+				
+					<?php include('agoodblock/code.php'); ?>
+				
+				<?php elseif( get_row_layout() == 'brodtext'): ?>
 			
-			<?php include('agoodblock/2-col-wysiwyg.php'); ?>
-		
-		<?php elseif( get_row_layout() == 'citat' ): ?>
-		
-			<?php include('agoodblock/citat.php'); ?>
-			
-		<?php elseif( get_row_layout() == 'code' ): ?>
-		
-			<?php include('agoodblock/code.php'); ?>
-		
-		<?php elseif( get_row_layout() == 'brodtext'): ?>
-	
-			<?php include('agoodblock/brodtext.php'); ?>
-		
-		<!-- fixa funktion för att  bädda in shortcodes -->
-		<?php elseif( get_row_layout() == 'shortcode'):
-		
-			// echo do_shortcode("[the_sub_field('shortcode');]"); ?>
-							
-		<?php elseif( get_row_layout() == 'video'): ?>
-		
-			<?php include('agoodblock/video.php'); ?>
-		
-		<?php elseif( get_row_layout() == 'smartslider'): ?>
-		
-			<?php include('agoodblock/smartslider.php'); ?>
-			
-		<?php elseif( get_row_layout() == 'module_name'): ?>
-		
-			<?php include('agoodblock/module-name.php'); ?>
-			
-		<?php elseif( get_row_layout() == 'hero'): ?>
-		
-			<?php include('agoodblock/hero.php'); ?>
-			
-		<?php elseif( get_row_layout() == 'spacer'): ?>
-		
-			<?php include('agoodblock/spacer.php'); ?>
-			
-		<?php elseif( get_row_layout() == 'typography'): ?>
-		
-			<?php include('agoodblock/typography.php'); ?>
-		
-		<?php elseif( get_row_layout() == 'video_2_col'): ?>
-		
-			<?php include('agoodblock/video-2-col.php'); ?>
+					<?php include('agoodblock/brodtext.php'); ?>
+				
+				<!-- fixa funktion för att  bädda in shortcodes -->
+				<?php elseif( get_row_layout() == 'shortcode'):
+				
+					// echo do_shortcode("[the_sub_field('shortcode');]"); ?>
+									
+				<?php elseif( get_row_layout() == 'video'): ?>
+				
+					<?php include('agoodblock/video.php'); ?>
+				
+				<?php elseif( get_row_layout() == 'smartslider'): ?>
+				
+					<?php include('agoodblock/smartslider.php'); ?>
+					
+				<?php elseif( get_row_layout() == 'module_name'): ?>
+				
+					<?php include('agoodblock/module-name.php'); ?>
+					
+				<?php elseif( get_row_layout() == 'hero'): ?>
+				
+					<?php include('agoodblock/hero.php'); ?>
+					
+				<?php elseif( get_row_layout() == 'spacer'): ?>
+				
+					<?php include('agoodblock/spacer.php'); ?>
+					
+				<?php elseif( get_row_layout() == 'typography'): ?>
+				
+					<?php include('agoodblock/typography.php'); ?>
+				
+				<?php elseif( get_row_layout() == 'video_2_col'): ?>
+				
+					<?php include('agoodblock/video-2-col.php'); ?>
 
-		<?php elseif( get_row_layout() == 'malin'): ?>
-		
-			<?php include('agoodblock/malin.php'); ?>
-		
-		<?php elseif( get_row_layout() == 'utmaning_losning'): ?>
-		
-			<?php include('agoodblock/utmaning-losning.php'); ?>
-		
-		<?php elseif( get_row_layout() == 'vimeo'): ?>
-		
-			<?php include('agoodblock/vimeo.php'); ?>
-			
-		<?php elseif( get_row_layout() == '3_col_25_50_25'): ?>
-		
-			<?php include('agoodblock/3-col-25-50-25.php'); ?>
+				<?php elseif( get_row_layout() == 'malin'): ?>
+				
+					<?php include('agoodblock/malin.php'); ?>
+				
+				<?php elseif( get_row_layout() == 'utmaning_losning'): ?>
+				
+					<?php include('agoodblock/utmaning-losning.php'); ?>
+				
+				<?php elseif( get_row_layout() == 'vimeo'): ?>
+				
+					<?php include('agoodblock/vimeo.php'); ?>
+					
+				<?php elseif( get_row_layout() == '3_col_25_50_25'): ?>
+				
+					<?php include('agoodblock/3-col-25-50-25.php'); ?>
 			
         <?php endif; ?>
     <?php endwhile; ?>
--- wp-content/themes/astra-child/agoodblock/code.php
+++ wp-content/themes/astra-child/agoodblock/code.php
@@ -2,12 +2,12 @@
 	<div class="agi-outer">
 		<div class="agi-inner">
 			<code class="<?php the_sub_field('code_class'); ?>">
-	<pre>
-	<?php
-	$sub_field = get_sub_field_object('code');
-	echo htmlspecialchars($sub_field['value']);
-	?>
-	</pre>
+				<pre>
+<?php
+$sub_field = get_sub_field_object('code');
+echo htmlspecialchars($sub_field['value']);
+?>
+				</pre>
 			</code>
 		</div>
 	</div>	
