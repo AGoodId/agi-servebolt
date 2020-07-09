d3eee39fd6e847f6a923acc379cbbde23a1937e9
Mats Ingerdal <mats@agoodid.se>
2020-07-09 20:00:31 +0200

    responsive videos
    Signed-off-by: Mats Ingerdal <mats@agoodid.se>

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -70,3 +70,11 @@ function add_custom_pt( $query ) {
 }
 add_action( 'pre_get_posts', 'add_custom_pt' );
 
+/*  Add responsive container to embeds
+/* ------------------------------------ */ 
+function alx_embed_html( $html ) {
+    return '<div class="video-container">' . $html . '</div>';
+}
+ 
+add_filter( 'embed_oembed_html', 'alx_embed_html', 10, 3 );
+add_filter( 'video_embed_html', 'alx_embed_html' ); // Jetpack
